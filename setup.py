d6cb3b43c48923580e4105f64342217b436e3322
Tyler <tscizzle@gmail.com>
2016-09-13 20:25:03 -0400

    incremented to version 0.0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='graphpy',
-    version='0.0.6',
+    version='0.0.7',
     description='A Python implementation of a edges, vertices, and graphs',
     long_description=long_description,
     url='https://github.com/tscizzle/graphpy',
